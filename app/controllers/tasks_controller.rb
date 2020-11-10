d5df21b56aca3aa5dbdd74079533bebd98099de8
Louis Paulsmeier <louis-paulsmeier@hotmail.de>
2020-11-10 15:35:08 +0100

    Rails new Taskmanager not styled jet

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,7 +31,6 @@ class TasksController < ApplicationController
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-
     # no need for app/views/tasks/destroy.html.erb
     redirect_to tasks_path
   end
